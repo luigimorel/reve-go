1464a8e6d9c9ffe19dac8d5321ce874848f858b2
Luigi Morel <48476293+luigimorel@users.noreply.github.com>
2025-10-11 23:17:12 +0300

    add example + make interface clearer (#1)
    * add reve-go create image example
    
    * fix: make interface clearer

---
+++ example/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"log"
+	"log/slog"
+	"os"
+
+	"github.com/joho/godotenv"
+	revego "github.com/luigimorel/reve-go"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	client, err := revego.NewClient(
+		os.Getenv("API_KEY"))
+	if err != nil {
+		panic(err)
+	}
+
+	req := revego.CreateImageRequest{
+		Prompt:      "Create a cart",
+		AspectRatio: "16:9",
+	}
+
+	resp, err := client.CreateImage(context.Background(), req)
+	if err != nil {
+		panic(err)
+	}
+
+	slog.Debug("info", "image generated", resp)
+}
